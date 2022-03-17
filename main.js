409ad459ede4450718b806842ea635996c26d1cb
fatemanasrinmitu-323 <96509453+fatemanasrinmitu-323@users.noreply.github.com>
2022-03-17 17:17:22 +0600

    added html css and jquery plugin

---
+++ main.js
@@ -0,0 +1,22 @@
+/* owl js */
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+window.html5 = {
+	'shivCSS': false
+};
